95d52b02413da6e68ed4805edc6e02aac97399eb
Lamgok <142557192+Lamgok@users.noreply.github.com>
2026-05-04 23:42:58 +0700

    push

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter_windows_3.41.2-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Semester 6\\smk_sigumpar_mobile" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter_windows_3.41.2-stable\\flutter"
+  "PROJECT_DIR=D:\\Semester 6\\smk_sigumpar_mobile"
+  "FLUTTER_ROOT=D:\\flutter_windows_3.41.2-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Semester 6\\smk_sigumpar_mobile\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Semester 6\\smk_sigumpar_mobile"
+  "FLUTTER_TARGET=D:\\Semester 6\\smk_sigumpar_mobile\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049OTA2NzNhNGVlZg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NmMwYmFhZWJmNw==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS4w"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Semester 6\\smk_sigumpar_mobile\\.dart_tool\\package_config.json"
+)
