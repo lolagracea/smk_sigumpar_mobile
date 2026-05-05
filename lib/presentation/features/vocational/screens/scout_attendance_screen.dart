195219d1c0bbb73a92bf81d6cd5bdbfbaa941373
ivana <ivanapasaribu17@gmail.com>
2026-05-05 11:26:37 +0700

    update pramuka

--- lib/presentation/features/vocational/screens/scout_attendance_screen.dart
+++ lib/presentation/features/vocational/screens/scout_attendance_screen.dart
@@ -3,3 +3,13 @@ import 'pramuka/absensi_pramuka_screen.dart';
 
 // Re-export agar router yang sudah ada tetap berfungsi
 export 'pramuka/absensi_pramuka_screen.dart' show AbsensiPramukaScreen;
+
+// Alias ScoutAttendanceScreen → AbsensiPramukaScreen
+class ScoutAttendanceScreen extends StatelessWidget {
+  const ScoutAttendanceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AbsensiPramukaScreen();
+  }
+}
